73ca3d42f485ae8a1b1b59b3a0abe2b9e03f0061
Vytautas Antanavičius <vytautas.a@ito.lt>
2015-12-18 06:28:41 +0200

    Change url of api and client

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -5,8 +5,8 @@
     angular
         .module('beerLoggerApp')
         .constant('moment', moment)
-        .constant('api', 'http://backend.restapi.dev/api')
-        .constant('api_oauth', 'http://backend.restapi.dev/oauth/v2')
+        .constant('api', 'http://api.beerlogger.tru.lt/api')
+        .constant('api_oauth', 'http://api.beerlogger.tru.lt/oauth/v2')
         .constant('client_id', '1_5qejn38rtukggw4w4ko0sk0okcgsk4c4s0g0cc8c40wcok0w4g')
         .constant('client_secret', '4zom9re3vlogo0og8scggc8cc0wok8k88okkk8wcwo4sc4ccos');
 
